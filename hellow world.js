96b8bad02fb7f18b86036e7fff164fd7c0595dd9
keryfire <46861082+keryfire@users.noreply.github.com>
2023-04-27 23:20:56 +0800

    Update hellow world.js

--- hellow world.js
+++ hellow world.js
@@ -1,12 +1,4 @@
-// 显示控制台
-if (SHOW_CONSOLE) {
-    console.show();
-    sleep(300);
-    // 修改控制台位置
-    console.setPosition(0, 100);
-    // 修改控制台大小
-    console.setSize(device.width, device.height / 4);
-}
+
 
 // 在控制台输出 Hello Hamibot
 toastLog("Hello Hamibot");
