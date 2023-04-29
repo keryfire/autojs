db84e3bf71931c8aa4471f1806ff2351b5beed29
keryfire <46861082+keryfire@users.noreply.github.com>
2023-04-29 14:05:27 +0800

    Update hellow world.js

--- hellow world.js
+++ hellow world.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// 显示控制台
-=======
->>>>>>> 96b8bad02fb7f18b86036e7fff164fd7c0595dd9
 
-
-// 在控制台输出 Hello Hamibot
-toastLog("Hello Hamibot");
+// 在控制台输出 Hello world
+toastLog("Hello world");
